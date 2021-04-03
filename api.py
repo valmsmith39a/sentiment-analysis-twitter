51e643c0105b73124abb5ffb4f35da1ba36d5533
valmsmith39a <valmsmith39a@gmail.com>
2021-04-02 21:05:32 -0700

    twitter api call - get tweets based on search term

--- api.py
+++ api.py
@@ -1,6 +1,10 @@
 import flask
+import os 
+import requests
+from dotenv import load_dotenv
 from flask import request, jsonify
 from prediction import predict
+load_dotenv()
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -9,8 +13,13 @@ def home():
     return '''<h1>Sentiment Analysis - Twitter </h1>'''
 
 @app.route('/sentiment', methods=['GET'])
-def stock_info():
+def sentiment():
     search_term = request.args.get('search_term')
     sentiment_prediction = predict(search_term)
-    return jsonify(sentiment_prediction)
+    URL = "https://api.twitter.com/2/tweets/search/recent?query=cupcakes&tweet.fields=created_at&max_results=100"
+    BEARER_TOKEN = os.getenv('BEARER_TOKEN')
+    auth_token = "Bearer" + " " + BEARER_TOKEN
+    headers_auth = {"Authorization": auth_token}
+    r = requests.get(URL, headers=headers_auth)
+    return r.json()
 app.run()
--- prediction.py
+++ prediction.py
@@ -1,2 +1,5 @@
 def predict(data):
+    # call prepare_data
+    # twitter API 
+    # return prediction result                  
     return { 'id': '01', 'search_term': data, 'result': 'negative' } 
